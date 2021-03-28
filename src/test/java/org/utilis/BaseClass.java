f882639a30ae41406123bc4ce9314970248b419c
waltervetrivel737 <waltervetrivel737@gmail.com>
2021-03-28 18:05:18 +0530

    28/3 6pm

--- src/test/java/org/utilis/BaseClass.java
+++ src/test/java/org/utilis/BaseClass.java
@@ -1,6 +1,7 @@
 package org.utilis;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,16 +9,25 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -67,6 +77,10 @@ public static WebDriver dri;
 	public static void towait() {
 		dri.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
+	public static void moveto(WebElement element) {
+		Actions a=new Actions(dri);
+		a.moveToElement(element).perform();
+	}
 	
 	public static void intoframe(int sindex) {
 		dri.switchTo().frame(sindex);
@@ -101,6 +115,57 @@ public static WebDriver dri;
 		return v1;
 	}
 	
+	public static String value;
+
+	public static String fetchAllExcell(String fpath, String Sname) throws IOException {
+		File f = new File(fpath);
+		FileInputStream ip = new FileInputStream(f);
+		Workbook bk = new XSSFWorkbook(ip);
+		Sheet sh = bk.getSheet(Sname);
+		for (int i = 0; i < sh.getPhysicalNumberOfRows(); i++) {
+			Row r = sh.getRow(i);
+			for (int j = 0; j < r.getPhysicalNumberOfCells(); j++) {
+				Cell c = r.getCell(j);
+				int ct = c.getCellType();
+				if (ct == 1) {
+					value = c.getStringCellValue();
+				} else if (DateUtil.isCellDateFormatted(c)) {
+					Date date = c.getDateCellValue();
+					SimpleDateFormat sim = new SimpleDateFormat("dd/MM/yy");
+					value = sim.format(date);
+				} else {
+					double d = c.getNumericCellValue();
+					long l = (long) d;
+					value = String.valueOf(l);
+
+				}
+
+			}
+
+		}
+		return value;
+	}
+	public static String fetchSingleExcell(String fpath,String Sname,int rindex,int cindex) throws IOException {
+		File f = new File(fpath);
+		FileInputStream ip = new FileInputStream(f);
+		Workbook bk = new XSSFWorkbook(ip);
+		Sheet sh = bk.getSheet(Sname);
+		Row r = sh.getRow(rindex);
+		Cell cell = r.getCell(cindex);
+		int cc = cell.getCellType();
+		if (cc == 1) {
+			value = cell.getStringCellValue();
+		} else if (DateUtil.isCellDateFormatted(cell)) {
+			Date date = cell.getDateCellValue();
+			SimpleDateFormat sim = new SimpleDateFormat("dd/MM/yy");
+			value = sim.format(date);
+		} else {
+			double d = cell.getNumericCellValue();
+			long l = (long) d;
+			value = String.valueOf(l);
+		}
+		return value;
+	}
 
 
 }
